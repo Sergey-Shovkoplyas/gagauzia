c697e4dd90e3f114b9ea1ca14cf020de2ac6d12c
Sergey Shovkoplyas <shovkoplyassergey@gmail.com>
2020-03-24 06:15:59 +0200

    Home page

--- app/js/common.js
+++ app/js/common.js
@@ -1,11 +1,42 @@
-window.onload = function () {
-	
-	console.log('js is working!!!'); 
-
-}
-
-/* $( document ).ready(function() {
+$( document ).ready(function() {
 
 	console.log( 'JQuery!!!' );
 
-}); */
+	// -----------------------initialize sliders------------------
+	
+	$('.introduce__slider').slick({
+		dots: true,
+		arrows: false
+	});
+
+	$('.news__slider').slick({
+		dots: false,
+		arrows: true,
+		slidesToShow: 3,
+		responsive: [
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					centerMode: true,
+					
+				}
+			},
+			{
+				breakpoint: 660,
+				settings: {
+					slidesToShow: 1,
+					centerMode: true,
+					arrows: false
+				}
+			}
+		]
+	});
+
+});
